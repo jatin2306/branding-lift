400c49cd40da526dea27689d59a30f175f234e02
Jatin Gupta <jatingupta2306@gmail.com>
2026-08-13 22:26:08 +0530

    Update WidgetScript component to use hardcoded script and token values for improved functionality

--- src/lib/widget/WidgetScript.tsx
+++ src/lib/widget/WidgetScript.tsx
@@ -3,9 +3,9 @@ import Script from "next/script";
 export default function WidgetScript() {
   return (
     <Script
-      src={process.env.NEXT_PUBLIC_WIDGET_SCRIPT_URL}
-      data-employee-token={process.env.NEXT_PUBLIC_WIDGET_EMPLOYEE_TOKEN}
-      data-api-base={process.env.NEXT_PUBLIC_WIDGET_API_BASE}
+      src="https://liftbot.brandinglift.com/static/widget.js"
+      data-workspace-token="ngZ-Gdb_tbbhMCNqtJRsT8hprb8Dy1o7"
+      data-api-base="https://liftbot.brandinglift.com/api/widget"
       strategy="lazyOnload"
     />
   );
